8a7ca5e979b2aafe6ce34a749a690872e324e0a3
Tristan Grovender <therealtristangrovender@gmail.com>
2020-01-09 12:02:33 -0600

    wiring up redux-form the longer way

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+    // helper method
+    renderInput(formProps) {
+        // Pre-refactor
+        return (
+            <input
+                onChange={formProps.input.onChange}
+                value={formProps.input.value}
+            />
+        );
+    }
 
-export default StreamCreate;
+    render() {
+        return (
+            <form>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: "streamCreate"
+})(StreamCreate);
